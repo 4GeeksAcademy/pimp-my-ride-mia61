eea9225e2e0811546d1fd05c3cd9a0096ab31e17
Michael Mirisciotta <151809512+Michaelmira@users.noreply.github.com>
2024-03-28 20:01:26 +0000

    app.config fix bug, add secret key to app.py and .env

--- src/app.py
+++ src/app.py
@@ -28,6 +28,7 @@ if db_url is not None:
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 
+app.config["JWT_SECRET_KEY"] = os.environ.get("FLASK_APP_KEY")
 jwt = JWTManager(app)
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
