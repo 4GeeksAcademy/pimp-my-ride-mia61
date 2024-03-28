4bc7e6b7075eeab4c579b17032366326c54ac9fe
Michael Mirisciotta <151809512+Michaelmira@users.noreply.github.com>
2024-03-28 17:15:25 +0000

    navbar button to /log-in and route to layout /log-in

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light">
@@ -9,8 +10,8 @@ export const Navbar = () => {
 					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
 				</Link>
 				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
+					<Link to="/Log-in">
+						<button className="btn btn-primary">Log in</button>
 					</Link>
 				</div>
 			</div>
