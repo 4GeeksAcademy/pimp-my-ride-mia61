bd97b425d833efbd5fe88575a5f31180c4584cd0
Michael Mirisciotta <151809512+Michaelmira@users.noreply.github.com>
2024-04-03 23:46:45 +0000

    edit PUT in routes and fix work order model name

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
   
 import os
 from flask_admin import Admin
-from .models import db, User, Customer, Work_order, Comment
+from .models import db, User, Customer, WorkOrder, Comment
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -13,7 +13,7 @@ def setup_admin(app):
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
     admin.add_view(ModelView(Customer, db.session))
-    admin.add_view(ModelView(Work_order, db.session))
+    admin.add_view(ModelView(WorkOrder, db.session))
     admin.add_view(ModelView(Comment, db.session))
 
     # You can duplicate that line to add mew models
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User, Customer, Work_order, Comment
+from api.models import db, User, Customer, WorkOrder, Comment
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
@@ -100,7 +100,7 @@ def create_work_order():
     user = User.query.filter_by(id=user_id).one_or_none()
     if user is None:
         return jsonify({"msg": "A user with that id does not exist"}), 404
-    work_order = Work_order (user_id=user_id, customer_id=customer_id, wo_status=wo_status, make=make, model=model, color=color, vin=vin, license_plate=license_plate)
+    work_order = WorkOrder (user_id=user_id, customer_id=customer_id, wo_status=wo_status, make=make, model=model, color=color, vin=vin, license_plate=license_plate)
     db.session.add(work_order)
     db.session.commit()
     db.session.refresh(work_order)
@@ -110,37 +110,19 @@ def create_work_order():
 @api.route('/work-order/edit/<int:work_order_id>', methods=['PUT'])
 def edit_work_order(work_order_id):
     data = request.json
-    
     if not data:
         return jsonify({"msg": "No JSON data provided"}), 400
-    
-    user_id = data.get("user_id")
-    customer_id = data.get("customer_id")
     wo_status = data.get("wo_status")
     make = data.get("make")
     model = data.get("model")
     color = data.get("color") 
     vin = data.get("vin") 
     license_plate  = data.get("license_plate")
-
-    if None in (user_id, customer_id, wo_status, make, model, color, vin, license_plate):
+    if None in ( wo_status, make, model, color, vin, license_plate):
         return jsonify({"msg": "Some required fields are missing"}), 400 
-
-    work_order = Work_order.query.get(work_order_id)
-
+    work_order = WorkOrder.query.get(work_order_id)
     if work_order is None:
         return jsonify({"msg": "Work order not found"}), 404
-    
-    customer = Customer.query.get(customer_id)
-    if customer is None:
-        return jsonify({"msg": "A customer with that id does not exist"}), 404
-    
-    user = User.query.get(user_id)
-    if user is None:
-        return jsonify({"msg": "A user with that id does not exist"}), 404
-    
-    work_order.user_id = user_id 
-    work_order.customer_id = customer_id
     work_order.wo_status = wo_status
     work_order.make = make
     work_order.model = model
@@ -155,20 +137,20 @@ def edit_work_order(work_order_id):
 #  ?? Should be private ?? 
 @api.route('/work-order/all', methods=['GET'])
 def get_all_work_orders():
-    work_orders = Work_order.query.all()
+    work_orders = WorkOrder.query.all()
     serialized_work_orders = [wo.serialize() for wo in work_orders]
     return jsonify({"work_orders": serialized_work_orders}), 200
 
 @api.route('/work-order/<int:work_order_id>', methods=['GET'])
 def get_work_order(work_order_id):
-    work_order = Work_order.query.get(work_order_id)
+    work_order = WorkOrder.query.get(work_order_id)
     if work_order is None:
         return jsonify({"msg": "Work order not found"}), 404
     return jsonify({"work_order": work_order.serialize()}), 200
 
 @api.route('/work-order/delete/<int:work_order_id>', methods =['DELETE'])
 def delete_work_order(work_order_id):
-    work_order = Work_order.query.get(work_order_id)
+    work_order = WorkOrder.query.get(work_order_id)
 
     if work_order is None:
         return jsonify({"msg": "work order not found" }), 404
@@ -177,8 +159,6 @@ def delete_work_order(work_order_id):
     db.session.commit()
 
     return jsonify({"msg": "Work order successfully deleted"}), 200
-
-
 
 @api.route('/private', methods=['GET'])
 @jwt_required()
