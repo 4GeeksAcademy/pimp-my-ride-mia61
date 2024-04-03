2a5232900e010858c7fe931461ce717cf2f5b51a
Michael Mirisciotta <151809512+Michaelmira@users.noreply.github.com>
2024-04-03 21:39:13 +0000

    Createe get for work order all, and by ID and Put for edit

--- src/api/routes.py
+++ src/api/routes.py
@@ -71,8 +71,15 @@ def get_customer(cust_id):
     customer = Customer.query.filter_by(id=cust_id).first()
     if customer is None:
         return jsonify({"msg": "No customer found"}), 404
-    return jsonify({"customer":customer.serialize()}), 20
+    return jsonify({"customer":customer.serialize()}), 200
 
+@api.route('/work_orders/customer/<int:cust_id>', methods=['GET'])
+def get_work_orders_by_customer(cust_id):
+    customer = Customer.query.filter_by(id = cust_id).first()
+    if customer is None:
+        return jsonify({"msg": "Customer not found"}), 404
+    work_orders = [work_order.serialize() for work_order in customer.work_orders]
+    return jsonify(work_orders)
 
 # work order routes
 @api.route('/work-order/new', methods=['POST'])
@@ -93,14 +100,64 @@ def create_work_order():
     user = User.query.filter_by(id=user_id).one_or_none()
     if user is None:
         return jsonify({"msg": "A user with that id does not exist"}), 404
-    
-    
     work_order = Work_order (user_id=user_id, customer_id=customer_id, wo_status=wo_status, make=make, model=model, color=color, vin=vin, license_plate=license_plate)
     db.session.add(work_order)
     db.session.commit()
     db.session.refresh(work_order)
     response_body = {"msg": "Work Order succesfully created!", "work_order":work_order.serialize()}
     return jsonify(response_body), 201
+
+@api.route('/work-order/edit/<int:work_order_id>', methods=['PUT'])
+def edit_work_order(work_order_id):
+    user_id = request.json.get("user_id", None)
+    customer_id = request.json.get("customer_id", None)
+    wo_status = request.json.get("wo_status", None)
+    make = request.json.get("make", None)
+    model = request.json.get("model", None)
+    color = request.json.get("color", None) 
+    vin = request.json.get("vin", None) 
+    license_plate  = request.json.get("license_plate", None)
+    work_order = Work_order.query.get(work_order_id)
+    if work_order is None:
+        return jsonify({"msg": "Work order not found"}), 404
+    if user_id is None or customer_id is None or wo_status is None or make is None or model is None or color is None or vin is None or license_plate  is None:
+        return jsonify({"msg": "Some required fields are missing"}), 400 
+    customer = Customer.query.filter_by(id=customer_id).one_or_none()
+    if customer is None:
+        return jsonify({"msg": "A customer with that id does not exist"}), 404
+    user = User.query.filter_by(id=user_id).one_or_none()
+    if user is None:
+        return jsonify({"msg": "A user with that id does not exist"}), 404
+    work_order.user_id = user_id 
+    work_order.customer_id = customer_id
+    work_order.wo_status = wo_status
+    work_order.make = make
+    work_order.model = model
+    work_order.color = color
+    work_order.vin = vin
+    work_order.license_plate = license_plate
+    db.session.commit()
+    db.session.refresh(work_order)
+    return jsonify({"work_order": work_order.serialize()}), 201
+
+
+#  ?? Should be private ?? 
+@api.route('/work-order/all', methods=['GET'])
+def get_all_work_orders():
+    work_orders = Work_order.query.all()
+    serialized_work_orders = [wo.serialize() for wo in work_orders]
+    return jsonify({"work_orders": serialized_work_orders}), 200
+
+@api.route('/work-order/<int:work_order_id>', methods=['GET'])
+def get_work_order(work_order_id):
+    work_order = Work_order.query.get(work_order_id)
+    if work_order is None:
+        return jsonify({"msg": "Work order not found"}), 404
+    return jsonify({"work_order": work_order.serialize()}), 200
+
+
+
+
 
 @api.route('/private', methods=['GET'])
 @jwt_required()
