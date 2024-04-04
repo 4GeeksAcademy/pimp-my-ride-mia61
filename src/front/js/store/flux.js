6cafc782adc99c8ca26b079d4f7e99dfac7a9eef
Michael Mirisciotta <151809512+Michaelmira@users.noreply.github.com>
2024-04-04 19:56:42 +0000

    erase un-used code from flux

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,27 +1,12 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			],
+			
 			token: undefined,
 			localStorageChecked: undefined
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
 
 			logIn: async (body) => {
 				const response = await fetch(
@@ -54,21 +39,6 @@ const getState = ({ getStore, getActions, setStore }) => {
 				});
 			},
 
-			fetchPrivateEndpoint: async () => {
-				const store = getStore();
-				const response = await fetch (
-					process.env.BACKEND_URL + "/api/private", {
-						headers: {
-							"Content-Type": "application/json",
-							"Authorization": "Bearer " + store.token
-						}
-					}
-				);
-				const body = await response.json();
-				setStore({
-					privateData: body
-				});
-			},
 
 			logUserOut: () => {
 				setStore({
@@ -79,33 +49,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 				}
 				console.log(getStore().token)
 			},
-
-			getMessage: async () => {
-				try{
-					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
-					const data = await resp.json()
-					setStore({ message: data.message })
-					// don't forget to return something, that is how the async resolves
-					return data;
-				}catch(error){
-					console.log("Error loading message from backend", error)
-				}
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
+			
 		}
 	};
 };
