b47907b19068cd89deb6c6f0b923d0d208921d88
Mariia Bratukhina <107720808+Marii2806@users.noreply.github.com>
2024-04-24 20:00:15 +0000

    Image size error message added, comments removew from CustomerWorkOrder

--- src/front/js/pages/CustomerWorkOrder.js
+++ src/front/js/pages/CustomerWorkOrder.js
@@ -96,7 +96,6 @@ export const CustomerWorkOrder = () => {
                     </div>
                 )}
             </div>
-            <WorkOrderComments writeAccess = {false}/>
         </div>
     );
 };
