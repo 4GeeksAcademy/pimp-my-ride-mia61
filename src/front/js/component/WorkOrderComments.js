92804b0fa16b2f30e5cd30d1ff6feaa38a1b6017
Michael Mirisciotta <151809512+Michaelmira@users.noreply.github.com>
2024-04-24 18:02:08 +0000

    Fix Progressbar add comments to work order details

---
+++ src/front/js/component/WorkOrderComments.js
@@ -0,0 +1,36 @@
+import React, { useContext, useState } from "react";
+import { Context } from "../store/appContext";
+import "../../styles/ProgressBar.css";
+
+export const WorkOrderComments = ({ comments, writeAccess }) => {
+  const { store, actions } = useContext(Context);
+
+
+
+
+
+  return (
+    <div>
+      <div class="mb-3">
+        <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+      </div>
+      <div>
+        <div class="list-group">
+          { comments?.map((comment, index) => (
+            <React.Fragment key={index}>
+              <a href="#" class="list-group-item list-group-item-action active" aria-current="true">
+                <div class="d-flex w-100 justify-content-between">
+                  
+                  <small>3 days ago</small>
+                </div>
+                <p class="mb-1">{comment.message}</p>
+              </a>
+            </React.Fragment>
+          ))}
+          
+        </div>  
+      </div>
+    </div>
+  );
+};
